a4724e642c09e166b3ae4debe2bd12732df7ad53
Renan Lopes Ferreira <relferreira@gmail.com>
2017-08-29 22:41:49 -0300

    Redux config

--- src/index.js
+++ src/index.js
@@ -3,15 +3,32 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
+import axiosMiddleware from 'redux-axios-middleware';
+import axios from 'axios';
+import registerServiceWorker from './registerServiceWorker';
 
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import reducer from './reducers';
+
+const client = axios.create({
+  baseURL: `${process.env.API_URL}`,
+  responseType: 'json'
+});
+
+const componseEnhancers =
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducer,
+  componseEnhancers(applyMiddleware(axiosMiddleware(client)))
+);
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 registerServiceWorker();
---
+++ src/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from 'redux';
+
+function reducer(state, action) {}
+
+export default combineReducers(reducer);
