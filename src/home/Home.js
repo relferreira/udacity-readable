26fc42210c60ff28873184f963365443e8044f60
Renan Lopes Ferreira <relferreira@gmail.com>
2017-09-04 21:13:00 -0300

    Select tag for sort and order, and menu links

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import { listCategories, listPosts } from './actions';
 
-export function orderPosts(posts, sortBy, order) {
+export function organizePosts(posts, sortBy, order, selectedCategory = null) {
   return posts
     .filter(post => !post.deleted)
+    .filter(
+      post => (selectedCategory ? post.category === selectedCategory : post)
+    )
     .sort(
       (post1, post2) =>
         order === 'asc'
@@ -14,34 +18,74 @@ export function orderPosts(posts, sortBy, order) {
     );
 }
 
+export function extractCategoryFromUrl(url) {
+  let matches = url.match(/\?category=(.*)/);
+  return matches ? matches[1] : null;
+}
+
 class Home extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       orderBy: 'desc',
-      sortBy: 'voteScore'
+      sortBy: 'voteScore',
+      selectedCategory: null
     };
   }
   componentDidMount() {
     this.props.listCategories();
     this.props.listPosts();
+    this.checkSelectedCategory(this.props);
   }
+
+  componentWillReceiveProps(nextProps) {
+    this.checkSelectedCategory(nextProps);
+  }
+
+  checkSelectedCategory = props => {
+    let category = extractCategoryFromUrl(props.location.search);
+    this.setState({ selectedCategory: category || null });
+  };
+
+  handleSortByChange = event => this.setState({ sortBy: event.target.value });
+
+  handleOrderByChange = event => this.setState({ orderBy: event.target.value });
+
   render() {
     const { categories, posts } = this.props.data;
-    const { orderBy, sortBy } = this.state;
-    const filteredPost = orderPosts(posts, sortBy, orderBy);
+    const { orderBy, sortBy, selectedCategory } = this.state;
+    const filteredPost = organizePosts(
+      posts,
+      sortBy,
+      orderBy,
+      selectedCategory
+    );
     return (
       <div>
         <h1>Home</h1>
-        <div className="menu">
-          <ul>
-            {categories.map((category, index) => (
-              <li key={index}>{category.name}</li>
-            ))}
-          </ul>
-        </div>
+        {!selectedCategory && (
+          <div className="menu">
+            <ul>
+              {categories.map((category, index) => (
+                <li key={index}>
+                  <Link to={`/?category=${category.name}`}>
+                    {category.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
         <div className="posts">
+          <select value={sortBy} onChange={this.handleSortByChange}>
+            <option value="voteScore">Vote score</option>
+            <option value="timestamp">Date</option>
+          </select>
+          <select value={orderBy} onChange={this.handleOrderByChange}>
+            <option value="asc">Ascendent</option>
+            <option value="desc">Descendent</option>
+          </select>
           {filteredPost.map((post, index) => (
             <div key={index}>
               <p>Title: {post.title}</p>
--- src/home/Home.test.js
+++ src/home/Home.test.js
@@ -1,4 +1,4 @@
-import { orderPosts } from './Home.js';
+import { organizePosts, extractCategoryFromUrl } from './Home.js';
 
 describe('Posts', () => {
   it('should filter deleted posts', () => {
@@ -6,7 +6,7 @@ describe('Posts', () => {
       { title: 'test', deleted: false },
       { title: 'test2', deleted: true }
     ];
-    let filteredPosts = orderPosts(posts);
+    let filteredPosts = organizePosts(posts);
     expect(filteredPosts.length).toEqual(1);
     expect(filteredPosts[0].title).toEqual('test');
   });
@@ -17,7 +17,7 @@ describe('Posts', () => {
       { title: 'test2', voteScore: 3 },
       { title: 'test3', voteScore: 2 }
     ];
-    let filteredPosts = orderPosts(posts, 'voteScore', 'desc');
+    let filteredPosts = organizePosts(posts, 'voteScore', 'desc');
     expect(filteredPosts[0].voteScore).toEqual(3);
     expect(filteredPosts[1].voteScore).toEqual(2);
     expect(filteredPosts[2].voteScore).toEqual(1);
@@ -29,7 +29,7 @@ describe('Posts', () => {
       { title: 'test2', voteScore: 3 },
       { title: 'test3', voteScore: 2 }
     ];
-    let filteredPosts = orderPosts(posts, 'voteScore', 'asc');
+    let filteredPosts = organizePosts(posts, 'voteScore', 'asc');
     expect(filteredPosts[0].voteScore).toEqual(1);
     expect(filteredPosts[1].voteScore).toEqual(2);
     expect(filteredPosts[2].voteScore).toEqual(3);
@@ -41,9 +41,27 @@ describe('Posts', () => {
       { title: 'test2', timestamp: 1467166872636 },
       { title: 'test3', timestamp: 1467166872635 }
     ];
-    let filteredPosts = orderPosts(posts, 'timestamp', 'desc');
+    let filteredPosts = organizePosts(posts, 'timestamp', 'desc');
     expect(filteredPosts[0].timestamp).toEqual(1467166872636);
     expect(filteredPosts[1].timestamp).toEqual(1467166872635);
     expect(filteredPosts[2].timestamp).toEqual(1467166872634);
+  });
+
+  it('should filter posts by selected category', () => {
+    let posts = [
+      { title: 'test', category: 'react' },
+      { title: 'test2', category: 'redux' }
+    ];
+    let filteredPosts = organizePosts(posts, 'voteScore', 'desc', 'react');
+    expect(filteredPosts.length).toEqual(1);
+    expect(filteredPosts[0].category).toEqual('react');
+  });
+});
+
+describe('Url extraction', () => {
+  it('should get category from URL', () => {
+    expect(
+      extractCategoryFromUrl('https://localhost:8080/?category=react')
+    ).toEqual('react');
   });
 });
