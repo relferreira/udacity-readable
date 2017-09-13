e8e13e6601062acf84d95794b05d98f15736bf7d
Renan Lopes Ferreira <relferreira@gmail.com>
2017-09-13 00:56:46 -0300

    Header adjustment

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import glamorous, { H1, Div } from 'glamorous';
 import PropTypes from 'prop-types';
 
 const HeaderContainer = glamorous.div({
   display: 'flex',
   alignItems: 'center',
-  padding: '5px 16px'
+  padding: '5px 16px',
+  '& a': {
+    textDecoration: 'none',
+    color: 'inherit'
+  }
 });
 
 const Header = ({ title, children }) => {
   return (
     <HeaderContainer>
       <H1 fontSize={30} flex={1}>
-        {title}
+        <Link to="/">{title}</Link>
       </H1>
       <Div position="absolute" right="16px">
         {children}
